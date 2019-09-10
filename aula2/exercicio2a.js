5e16bfe60f8a593ab724c27aaa28b96cac7b1bf2
Thalita <thalita.andrade287@gmail.com>
2019-09-10 13:48:36 -0300

    Excluíndo pastas

--- aula2/exercicio2a.js
+++
@@ -1,9 +0,0 @@
-// EXERCÍCIO 2 a)
-
-const fahrenheit = prompt("Qual o valor em Fahrenheit?");
-
-const fahrEmKelvin = (fahrenheit - 32) * 5/9 + 273.15;
-console.log("Kelvin: "+ fahrEmKelvin);
-
-const fahrEmCelsius = (fahrenheit - 32) * 5/9;
-console.log("Celsius: "+ fahrEmCelsius);
--- aula2/exercicio3a.js
+++
@@ -1,9 +0,0 @@
-//EXERCÍCIO 3 a)
-
-const casaDestrancada = confirm("A Casa está destrancada?");
-
-const chave = confirm("Você tem a chave da casa?");
-
-const consegueEntrarNaCasa = casaDestrancada || chave;
-
-console.log("O usuário consegue entrar na casa? " + consegueEntrarNaCasa)
