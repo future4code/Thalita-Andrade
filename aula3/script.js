602443cbacc58469fe6d32ac638cd22e15c3d4d2
Thalita Andrade <thalita.andrade287@gmail.com>
2019-09-25 18:13:09 -0300

    Passo 5

--- aula3/script.js
+++ aula3/script.js
@@ -1,42 +1,45 @@
 class Post {
-    constructor(titulo, autor, conteudo) {
-        this.titulo = titulo
-        this.autor = autor
-        this.conteudo = conteudo
+    constructor() {
+        this.titulo = document.getElementById("titulo").value;
+        this.autor = document.getElementById("autor").value;
+        this.conteudo = document.getElementById("conteudo").value;
     }
 }
 
 let listaDePost = [];
 
 function criarPost(){
-    const titulo = document.getElementById("titulo").value;
-    const autor = document.getElementById("autor").value;
-    const conteudo = document.getElementById("conteudo").value;
 
-    const post = new Post(titulo, autor,conteudo);
+    const post = new Post();
 
     listaDePost.push(post);
-
-    console.log(listaDePost)
 
     if(document.getElementById("titulo").value!="") {
         document.getElementById("titulo").value=""
     }
+
     if(document.getElementById("conteudo").value!="") {
         document.getElementById("conteudo").value=""
     }
+
     if(document.getElementById("autor").value!="") {
         document.getElementById("autor").value=""
     }
 }
 
-const container = document.querySelector(".container");
+function postCriados(){
 
-for(let post of listaDePost) {
-    container.innerHTML += '<div>' +
-                                '<p> Título: ' + post.titulo + '</p>' +
-                                '<p> Autor: ' + post.autor + '</p>' +
-                                '<p> Conteúdo: ' + post.conteudo + '</p>' +
-                            '</div>'
+    criarPost();
+
+    let container = document.querySelector(".container");
+
+    container.innerHTML = "";
+
+    for(let post of listaDePost) {
+        container.innerHTML += '<div>' +
+                                    '<p> ' + post.titulo + '</p>' +
+                                    '<p> ' + post.autor + '</p>' +
+                                    '<p> ' + post.conteudo + '</p>' +
+                                '</div>'
+    }
 }
-
