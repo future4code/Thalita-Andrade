11b938f51eda6c416c4cf78150386b9391cf2fe8
Thalita Andrade <thalita.andrade287@gmail.com>
2019-09-26 15:58:08 -0300

    passo 2

---
+++ aula4/script.js
@@ -0,0 +1,37 @@
+
+
+function limparInput() {
+    if(document.getElementById("input-valorGasto").value!="") {
+        document.getElementById("input-valorGasto").value=""
+    }
+
+    if(document.getElementById("input-descricaoDoGasto").value!="") {
+        document.getElementById("input-descricaoDoGasto").value=""
+    }
+}
+
+class Despesa {
+    constructor(valor, tipo, descricao){
+        this.valor = valor
+        this.tipo = tipo
+        this.descricao = descricao
+    }
+}
+
+let todosGastos = [];
+
+function salvarGasto() {
+
+    let valor = document.getElementById("input-valorGasto").value;
+    let tipo = document.getElementById("input-tipoDeGasto").value;
+    let descricao = document.getElementById("input-descricaoDoGasto").value;
+
+
+    const gastos = new Despesa(valor, tipo, descricao);
+
+    todosGastos.push(gastos);
+
+    limparInput() 
+
+    console.log("todosOsGastos", todosGastos)
+}
