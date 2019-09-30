31e2d56c68d31b53efcbb35c5d108352dc694d7d
Thalita Andrade <thalita.andrade287@gmail.com>
2019-09-30 16:00:11 -0300

    parte 1

--- aula1-react/src/App.js
+++ aula1-react/src/App.js
@@ -4,22 +4,64 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+    <div className="container">
+      <header>
+        <h1>FutureTube</h1>
+        <input placeholder="Buscar"></input>
       </header>
+      <section className="container-interno">
+        <aside>
+          <ul>
+            <li>Início</li>
+            <li>Em alta</li>
+            <li>Incrições</li>
+            <li>originais</li>
+            <li>Biblioteca</li>
+            <li>Histórico</li>
+          </ul>
+        </aside>
+        <div className="link-videos">
+          <ul>
+            <li>
+              <img src="https://picsum.photos/id/129/200/200"></img>
+              <p>Título vídeo</p>
+            </li>
+            <li>
+              <img src="https://picsum.photos/id/129/200/200"></img>
+              <p>Título vídeo</p>
+            </li>
+            <li>
+              <img src="https://picsum.photos/id/129/200/200"></img>
+              <p>Título vídeo</p>
+            </li>
+            <li>
+              <img src="https://picsum.photos/id/129/200/200"></img>
+              <p>Título vídeo</p>
+            </li>
+            <li>
+              <img src="https://picsum.photos/id/129/200/200"></img>
+              <p>Título vídeo</p>
+            </li>
+            <li>
+              <img src="https://picsum.photos/id/129/200/200"></img>
+              <p>Título vídeo</p>
+            </li>
+            <li>
+              <img src="https://picsum.photos/id/129/200/200"></img>
+              <p>Título vídeo</p>
+            </li>
+            <li>
+              <img src="https://picsum.photos/id/129/200/200"></img>
+              <p>Título vídeo</p>
+            </li>
+          </ul>
+
+        </div>
+      </section>
+      <footer>Oi eu moro no footer!</footer>
     </div>
+ 
   );
 }
 
