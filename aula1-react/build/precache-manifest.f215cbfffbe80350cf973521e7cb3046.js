7ebbf3be056a46f3b0f111ae15b4e6d2f114108c
Thalita Andrade <thalita.andrade287@gmail.com>
2019-10-03 12:05:57 -0300

    atualizando

---
+++ aula1-react/build/precache-manifest.f215cbfffbe80350cf973521e7cb3046.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119fd0163e03bb844457cc1dbdfe3e4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9eecf3788a4706d0eda",
+    "url": "/static/css/main.5367f468.chunk.css"
+  },
+  {
+    "revision": "9def4d7b52c1d7011bad",
+    "url": "/static/js/2.71e812c6.chunk.js"
+  },
+  {
+    "revision": "c9eecf3788a4706d0eda",
+    "url": "/static/js/main.3891a688.chunk.js"
+  },
+  {
+    "revision": "6ba24480ede1851694c2",
+    "url": "/static/js/runtime-main.cf2a1427.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
---
+++ aula3-react/build/precache-manifest.3a0fa0396e94ca566b93327217d86a7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c021e25799e9dfec89f61e0b2d03e92c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf7203e499ffb3e05690",
+    "url": "/static/css/main.27d5ef90.chunk.css"
+  },
+  {
+    "revision": "f7d1338c606171d6963d",
+    "url": "/static/js/2.6d6a1ef8.chunk.js"
+  },
+  {
+    "revision": "bf7203e499ffb3e05690",
+    "url": "/static/js/main.75f9da61.chunk.js"
+  },
+  {
+    "revision": "e518bfbbe517acc1101d",
+    "url": "/static/js/runtime-main.695fb48c.js"
+  }
+]);
