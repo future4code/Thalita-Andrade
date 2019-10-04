cef3a0a9b986256daa48b9cac64bd69549a8af29
Thalita Andrade <thalita.andrade287@gmail.com>
2019-10-04 19:54:15 -0300

    organizando pastas

---
+++ semana-4/aula4-react/build/precache-manifest.933c6d88cf0eec27673df886bd660ebb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "898971c910f3165255f804a8d028b3ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8a645c60184fde30459",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a11c2780e1fa1fd3e10a",
+    "url": "/static/js/2.ab2a4391.chunk.js"
+  },
+  {
+    "revision": "b8a645c60184fde30459",
+    "url": "/static/js/main.8049c862.chunk.js"
+  },
+  {
+    "revision": "ddb44f235a10712c0481",
+    "url": "/static/js/runtime-main.06ee43f4.js"
+  }
+]);
