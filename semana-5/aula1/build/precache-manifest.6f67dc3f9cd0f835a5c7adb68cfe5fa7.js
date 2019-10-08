522e2ca3adf63023d2521b65d3e845b3e1b894f7
Thalita Andrade <thalita.andrade287@gmail.com>
2019-10-08 14:07:47 -0300

    adicionando nova branch aula2

---
+++ semana-5/aula1/build/precache-manifest.6f67dc3f9cd0f835a5c7adb68cfe5fa7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "365acfe2cb4a516582543484af75c9ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef43cf152769fabbdefa",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c2a8dc225e57dee881f0",
+    "url": "/static/js/2.2cd04012.chunk.js"
+  },
+  {
+    "revision": "ef43cf152769fabbdefa",
+    "url": "/static/js/main.cbf0fdaa.chunk.js"
+  },
+  {
+    "revision": "b8092261b5652fa094ba",
+    "url": "/static/js/runtime-main.046870c0.js"
+  }
+]);
