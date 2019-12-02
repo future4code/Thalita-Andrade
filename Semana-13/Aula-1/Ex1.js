0558f3e273d54737869b21bc018a003ade118fb9
Thalita Andrade <thalita.andrade287@gmail.com>
2019-12-02 14:48:17 -0300

    Exercício 1 concluído

--- Semana-13/Aula-1/Ex1.js
+++ Semana-13/Aula-1/Ex1.js
@@ -1,10 +1,22 @@
 // Crie uma aplicação Node que recebe uma string representado uma operação e dois valores 
 // e retorna o resultado da operação selecionada utilizando os 2 valore
 
-const operation = ;
-const firstValue = ;
-const secondValue = ;
+const operation = process.argv[2];
+const firstValue = Number(process.argv[3]);
+const secondValue = Number(process.argv[4]);
 
-const result = firstValue, operation, secondValue;
+if (operation === "add") {
+    const result = firstValue + secondValue;
+    console.log(`O resultado é: ${result}`);
+} else if (operation === "sub") {
+    const result = firstValue - secondValue;
+    console.log(`O resultado é: ${result}`);
+} else if (operation === "mult") {
+    const result = firstValue * secondValue;
+    console.log(`O resultado é: ${result}`);
+} else if (operation === "div") {
+    const result = firstValue / secondValue;
+    console.log(`O resultado é: ${result}`);
+} 
 
-console.log(`O resultado é: ${result}`);
+
