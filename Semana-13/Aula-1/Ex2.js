86922db71f8ecc95c0a651d50b988b7ab04f54e5
Thalita Andrade <thalita.andrade287@gmail.com>
2019-12-02 12:10:22 -0300

    Aula 1 - Node.Js e Package.Json

---
+++ Semana-13/Aula-1/Ex2.js
@@ -0,0 +1,4 @@
+// Crie uma aplicação Node que receba uma string representando o nome da lista de tarefas 
+// e uma string representando uma nova tarefa, em seguida o programa deve adicionar a nova tarefa 
+// em um arquivo que tenha o nome da lista de tarefas.
+
