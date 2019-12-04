45607e6671bba5822cae259a8339a493d65784ab
Thalita Andrade <thalita.andrade287@gmail.com>
2019-12-04 13:00:00 -0300

    Exercício da Tarde - Aprofundamento em Promises

---
+++ Aula-2/src/Ex1.ts
@@ -0,0 +1,14 @@
+// Faça uma função que receba um array de números e retorne um objeto que
+// tenha as seguintes informações: a quantidade de números que há no array,
+// a quantidade de números ímpares no array e a soma de todos os elementos do array
+
+const numbers = [1,2,3,4,5,6,7,8,9,10]
+
+function resultNumbers(allNumbers: numbers[]){
+    return allNumbers.filter((el: number) => {
+        return 
+        //Quantidade de numeros que esta no array
+        //Quantidade de numeros ímpares
+        //Soma de todos os numeros
+    })
+}
