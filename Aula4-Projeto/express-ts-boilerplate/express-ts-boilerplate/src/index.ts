71052936db7874b8b7a01d2ad1ca83573ef847a5
Thalita Andrade <thalita.andrade287@gmail.com>
2019-12-20 14:59:42 -0300

    Editar usuário

--- Aula4-Projeto/express-ts-boilerplate/express-ts-boilerplate/src/index.ts
+++ Aula4-Projeto/express-ts-boilerplate/express-ts-boilerplate/src/index.ts
@@ -59,6 +59,51 @@ app.post('/mirror/:cor', (req: Request, res: Response) => {
   res.send(responseBody);
 });
 
+// CT - Ex1
+app.get('/getAllBrazilianClients', async (req: Request, res: Response) => {
+  const query = connection.select('primeiro_nome')
+                          .from('correntistas')
+                          .where('pais', 'Brazil');
+  const result = await query;
+  res.send(result);
+});
+// CT - Ex2
+app.get('/searchClientByName', (req: Request, res: Response) => {
+  const nameToSearch = req.query.name;
+  if(!nameToSearch){
+    res.status(400).end();
+    return;
+  }
+  const query = connection.raw(`SELECT primeiro_nome FROM correntistas WHERE primeiro_nome LIKE "%${nameToSearch}%"`)
+  query.then(result => {
+    res.send(result)
+  })
+});
+// CT - Ex3
+app.get('/getBirthdaysAfterDate/:date', (req: Request, res: Response) => {
+  const dateToBeCompared = req.params.date;
+  const query = connection.raw(`SELECT primeiro_nome, aniversario FROM correntistas WHERE aniversario < "${dateToBeCompared}"`)
+  query.then(result => {
+    res.send(result);
+  })
+});
+// CT - Ex4
+app.post('/createClient', (req: Request, res: Response) => {
+  const newCliente = {
+    ...req.body,
+    divida_perdoada: 0,
+    conta_corrente: 0,
+  };
+  const query = connection('correntistas').insert(newCliente);
+  query.then(result => {
+    res.send(result);
+  }).catch(e => {
+    res.send(e);
+  })
+});
+
+// PROJETO TO DO LIST
+
 app.post('/createUser', (req: Request, res: Response) => {
   const newUser = {
       ...req.body,
@@ -71,6 +116,22 @@ app.post('/createUser', (req: Request, res: Response) => {
       res.send(e);
   })
 });
+
+app.put('/editUser:id', async(req: Request, res: Response) => {
+  
+  const newNickName = req.body.nickname;
+  const editUser = req.params.id
+
+  try {
+    const query = connection('register_user').where('id', '=', editUser). update({nickname: newNickName});
+
+    const result = await query;
+  } catch (er) {
+    res.sendStatus(500).end()
+  }
+  res.sendStatus(200).end()
+});
+
 
 
 // Trecho do código responsável por inicializar todas as APIs
