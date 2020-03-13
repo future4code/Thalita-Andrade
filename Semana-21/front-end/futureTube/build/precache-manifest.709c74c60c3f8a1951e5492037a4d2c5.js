b0be31e6404a09c084f284faf65f03fd99fa89e6
Thalita Andrade <thalita.andrade287@gmail.com>
2020-03-13 12:25:10 -0300

    Atualizando PR

---
+++ Semana-21/front-end/futureTube/build/precache-manifest.709c74c60c3f8a1951e5492037a4d2c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48836409177bc7cbdfc2b8e650dfa321",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70e0467958442dd9d848",
+    "url": "/static/js/2.4647b369.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.4647b369.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c80309f0a7cda5378fc5",
+    "url": "/static/js/main.003765c1.chunk.js"
+  },
+  {
+    "revision": "4ef72fb5daf6dd0abafe",
+    "url": "/static/js/runtime-main.c2b0d7d0.js"
+  },
+  {
+    "revision": "9b055be612a408b83fae6bd48c93d85a",
+    "url": "/static/media/iconLogin.9b055be6.png"
+  },
+  {
+    "revision": "0209236fb0a341a6da6f4d1de2097a47",
+    "url": "/static/media/logo.0209236f.png"
+  }
+]);
